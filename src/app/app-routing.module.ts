b1beb52482d9b76011213778808a243c45c4a273
Angel Rodriguez <klju784@gmail.com>
2022-02-09 18:01:14 -0400

    [ADD] Agregar rutas de front
    Se diferencian las rutas de los componentes utilizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './page/main/main.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: 'main' , component: MainComponent }
+  { path: 'main' , component: MainComponent },
+  { path: 'user' , component: LandingComponent }
 ];
 
 @NgModule({
