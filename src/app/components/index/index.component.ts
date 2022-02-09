a3f3614851b5688ec6d1bca55b852c8be11f1b48
Angel Rodriguez <klju784@gmail.com>
2022-02-09 18:06:38 -0400

    [ADD] Validar componente de login
    Se realizan validaciones y conexion con API del componente login

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { $ } from 'protractor';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ApiService } from "../../communication/api.service";
+import { NotifyComponent } from "../notify/notify.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -8,11 +11,49 @@ import { $ } from 'protractor';
 })
 export class IndexComponent implements OnInit {
   
-  constructor() { }
+  constructor(private api:ApiService, private formBuilder: FormBuilder, private route: Router) { }
+
+  public loginForm: FormGroup;
+  public submitted: boolean = false;
+  public request: boolean = false;
   
+  @ViewChild("notify") notify:NotifyComponent;
   @Output() change_view = new EventEmitter<boolean>();
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+
+      email:    ['', [Validators.required, Validators.email]],
+      password_login: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+  
+  onSubmit() {
+    this.submitted = true
+    this.request = true
+    if (this.loginForm.invalid) {
+      this.request = false
+      return false;
+    }
+    
+    this.api.loginUser({
+      email: this.f.email.value,
+      password: this.f.password_login.value
+    }).subscribe((r:any) => {
+      this.notify.showNotify("liveToastLogin", null)
+      sessionStorage.setItem("user", JSON.stringify(r.user))
+      setTimeout(() => {
+        this.route.navigate(['/user'])
+      }, 3000);
+    }, err => {
+      this.request = false
+      this.notify.showNotify("liveToastError", err.error.message)
+    })
+  }
   
   signin(): void {
     this.change_view.emit(false);
