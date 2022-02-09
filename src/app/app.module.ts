16f76f3b95e1c7627097833914416b84f13472a8
Angel Rodriguez <klju784@gmail.com>
2022-02-09 18:02:08 -0400

    [ADD] Modificar archivo de modulos
    Se incluyen todos los modulos necesarios para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,19 +8,26 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { MainComponent } from './page/main/main.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NotifyComponent } from './components/notify/notify.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     MainComponent,
-    SigninComponent
+    SigninComponent,
+    DashboardComponent,
+    LandingComponent,
+    NotifyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
